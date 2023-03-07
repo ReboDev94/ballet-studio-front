7b7a62224b0debed274a0498da863615b9d6a7bb
Rafael de Jesus <rrrrebolledohdz@gmail.com>
2023-03-07 13:04:18 -0600

    added custom colors

--- src/auth/pages/LoginPage.tsx
+++ src/auth/pages/LoginPage.tsx
@@ -1,9 +1,31 @@
-import React from 'react'
-
 const LoginPage = () => {
   return (
-    <div>LoginPage</div>
-  )
-}
+    <div className="flex justify-center items-center h-screen">
+      <button
+        className="
+            py-2.5
+            px-5
+            inline-block
+            text-sm
+            text-center
+          text-secondary-900
+            cursor-pointer
+            rounded-md
+          bg-primary-800
+          hover:bg-primary-700
+            font-medium
+            outline-none
+            focus:ring-4
+          focus:ring-secondary-900
+            transform
+            active:scale-95
+            transition-transform
+            "
+      >
+        login
+      </button>
+    </div>
+  );
+};
 
-export default LoginPage
+export default LoginPage;
--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -2,7 +2,33 @@
 module.exports = {
   content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: {
+          900: '#582020',
+          800: '#8c3232',
+          700: '#982649',
+        },
+        secondary: {
+          900: '#F2E2DF',
+        },
+        accent: {
+          900: '#0E7C7B',
+        },
+        success: {
+          900: '#aebe97',
+          800: '#c4d5aa',
+          700: '#DAEDBD',
+        },
+        warning: {
+          900: '#c2b663',
+          800: '#dbcc70',
+          700: '#f3e37c',
+        },
+        base: '#F2F2F2',
+        default: '#582020',
+      },
+    },
   },
-  plugins: [],
+  plugins: [require('@tailwindcss/forms'), require('@tailwindcss/line-clamp')],
 };
