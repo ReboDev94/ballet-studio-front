3500a0a8fdeb0965e017fd43a413b6d12fba24fa
Rafael De Jesus Rebolledo <>
2023-07-21 16:55:45 -0600

    configure storybook

---
+++ src/stories/components/Button.stories.ts
@@ -0,0 +1,30 @@
+import type { Meta, StoryObj } from '@storybook/react';
+import { Button } from '@/common/components';
+// More on how to set up stories at: https://storybook.js.org/docs/react/writing-stories/introduction
+const meta = {
+  title: 'UI/Button',
+  component: Button,
+  tags: ['autodocs'],
+  argTypes: {
+    // backgroundColor: { control: 'color' },
+  },
+} satisfies Meta<typeof Button>;
+
+export default meta;
+type Story = StoryObj<typeof meta>;
+
+// More on writing stories with args: https://storybook.js.org/docs/react/writing-stories/args
+export const Primary: Story = {
+  args: {
+    variant: 'primary',
+    children: 'Hola mundo',
+  },
+};
+
+export const Disabled: Story = {
+  args: {
+    disabled: true,
+    variant: 'primary',
+    children: 'Hola mundo',
+  },
+};
